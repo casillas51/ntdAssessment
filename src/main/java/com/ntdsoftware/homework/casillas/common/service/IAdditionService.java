9d1c65ea0c63cc9509ec0e78a485e04d16bbb713
Antonio Casillas <casillas51@gmail.com>
2024-09-11 12:30:40 -0600

    Feature. Update addition service
      * Update methods for addition service
      * Add unit test for addition service

--- src/main/java/com/ntdsoftware/homework/casillas/common/service/IAdditionService.java
+++ src/main/java/com/ntdsoftware/homework/casillas/common/service/IAdditionService.java
@@ -1,12 +1,27 @@
 package com.ntdsoftware.homework.casillas.common.service;
 
+import com.ntdsoftware.homework.casillas.common.controller.request.AdditionRequest;
 import com.ntdsoftware.homework.casillas.common.controller.response.OperationResultResponse;
 import com.ntdsoftware.homework.casillas.common.enums.OperationTypeEnum;
+import com.ntdsoftware.homework.casillas.common.exception.ArithmeticOperationException;
 
+/**
+ * Service interface for addition operations.
+ */
 public interface IAdditionService {
 
-    final OperationTypeEnum operationType = OperationTypeEnum.ADDITION;
+    /**
+     * The type of operation this service handles.
+     */
+    OperationTypeEnum operationType = OperationTypeEnum.ADDITION;
 
-    OperationResultResponse add(int userId, Double term1, Double term2);
-
+    /**
+     * Performs an addition operation.
+     *
+     * @param userId the ID of the user performing the operation
+     * @param additionRequest the request object containing the terms to be added
+     * @return the result of the addition operation
+     * @throws ArithmeticOperationException if any of the input values are null
+     */
+    OperationResultResponse add(int userId, AdditionRequest additionRequest);
 }
